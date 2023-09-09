6acac87a68735dc1c1a7798a82b480b3783c0563
Sheryf Habib <sheryf.534@gmail.com>
2023-09-09 06:13:27 +0100

    simple JWT installation

--- Backend/Api/views.py
+++ Backend/Api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
+#from .serializers import 
+from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.auth import get_user_model
+from rest_framework import status
+from rest_framework.views import APIView
+#from Background_Tasks.tasks import 
+from django.core.cache import cache
+from rest_framework.exceptions import NotFound
+from django.shortcuts import get_object_or_404
