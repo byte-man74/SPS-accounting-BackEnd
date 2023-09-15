3f0fa457284e193fa6c5deee5c0b510683d9335e
Sheryf Habib <sheryf.534@gmail.com>
2023-09-15 17:10:32 +0100

    school model created

--- Backend/Main/models.py
+++ Backend/Main/models.py
@@ -1,3 +1,13 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class School(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    email_address = models.EmailField(max_length=50)
+    logo = models.ImageField()
+
+    def __str__(self):
+        return self.name
+    
