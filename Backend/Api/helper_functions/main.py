26f5bc9623fab6f1dca3292ab7aca2d93fcbc1f6
Sheryf Habib <sheryf.534@gmail.com>
2023-09-23 10:06:29 +0100

    added the get school for user

--- Backend/Api/helper_functions/main.py
+++ Backend/Api/helper_functions/main.py
@@ -1,6 +1,12 @@
 from Main.models import School
 from Authentication.models import CustomUser
+from django.shortcuts import get_object_or_404
 
-
-def Get_school_for_user (user_id):
-    user_school_id = CustomUser.objects.get(id=user_id)
+def get_school_for_user(user_id):
+    try:
+        custom_user = get_object_or_404(CustomUser, id=user_id)
+        user_school = custom_user.school
+        return user_school
+    except CustomUser.DoesNotExist:
+        # Handle the case where the user does not exist
+        return None
