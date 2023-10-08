32677ac598ce58ac00478f420360c497414277b6
Sheryf Habib <sheryf.534@gmail.com>
2023-10-08 12:34:30 +0100

    update

--- Backend/Api/Api_pages/operations/serializers.py
+++ Backend/Api/Api_pages/operations/serializers.py
@@ -30,19 +30,19 @@ class OperationsAccountCashTransactionRecordSerializer(serializers.ModelSerializ
     particulars = serializers.CharField(source='particulars.name', read_only=True)
     class Meta:
         model = Operations_account_transaction_record
-        fields = ('time', 'amount', 'transaction_category', 'particulars', 'reason', 'name_of_reciever', 'status' )
+        fields = ('id', 'time', 'amount', 'transaction_category', 'particulars', 'reason', 'name_of_reciever', 'status' )
 
 
 class CashTransactionReadSerializer (serializers.ModelSerializer):
     particulars = serializers.CharField(source='particulars.name', read_only=True)
     class Meta:
         model = Operations_account_transaction_record
-        fields = ('time', 'amount', 'transaction_category', 'particulars', 'name_of_reciever', 'status' )
+        fields = ('id' , 'time', 'amount', 'transaction_category', 'particulars', 'name_of_reciever', 'status' )
 
 class CashTransactionWriteSerializer (serializers.ModelSerializer):
     class Meta:
         model = Operations_account_transaction_record
-        fields = ('time', 'amount', 'reason', 'particulars', 'name_of_reciever', 'status' )
+        fields = ('id' , 'time', 'amount', 'reason', 'particulars', 'name_of_reciever', 'status' )
 
 
 class CashTransactionDetailsSerializer (serializers.Serializer):
