866f8bef38aaf063132915c5e8f0308dfbc0680c
Sheryf Habib <sheryf.534@gmail.com>
2023-10-23 08:18:16 +0100

    bug fix circular import problem

--- Backend/Main/models/__init__.py
+++ Backend/Main/models/__init__.py
@@ -1,4 +1,4 @@
 from .account_models import *
 from .payment import *
-from school_operations_models import *
-from transaction_records import *
+from .school_operations_models import *
+from .transaction_records import *
