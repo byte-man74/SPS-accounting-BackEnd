6ab5324497dcccc7268f442feab183d7ede06347
Sheryf Habib <sheryf.534@gmail.com>
2023-11-02 08:03:57 +0100

    migration on banks

---
+++ Backend/Paystack/service.py
@@ -0,0 +1,21 @@
+'''
+    This file exist to give the paystack api support for other complex API
+'''
+import requests
+from django.conf import settings
+
+
+PUBLIC_KEY = settings.PAYSTACK_PUBLIC_KEY
+SECRET_KEY = settings.PAYSTACK_SECRET_KEY
+
+
+def get_banks_from_paystack ():
+    url = "https://api.paystack.co/bank"
+    headers = {
+        "Authorization": f"Bearer {SECRET_KEY}"
+    }
+
+
+    response = requests.get(url, headers=headers)
+
+    print(response)
