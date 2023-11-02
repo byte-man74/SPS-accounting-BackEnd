d2644a9075afa305cdc7699e603eda6f333ca92d
Sheryf Habib <sheryf.534@gmail.com>
2023-11-02 07:50:46 +0100

    added the models

--- Backend/Paystack/models.py
+++ Backend/Paystack/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Bank (models.Model):
+    name = models.CharField(max_length=50)
+    bank_code = models.CharField(max_length=10)
+    slug = models.CharField(max_length=50)
+    bank_type = models.CharField(max_length=40)
+    currency = models.CharField(max_length=10)
+
+
+    def __str__(self):
+        return self.name
+    
