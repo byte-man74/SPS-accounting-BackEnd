251ca0586ae7f3b9372512613cd5fef63b48b23c
Sheryf Habib <sheryf.534@gmail.com>
2023-11-11 15:11:48 +0100

    added permission check

--- Backend/Api/Api_pages/directors/operations.py
+++ Backend/Api/Api_pages/directors/operations.py
@@ -26,6 +26,7 @@ account_type = "DIRECTOR"
 
 
 class GetAllPayroll (APIView):
+    permission_classes = [IsAuthenticated]
     '''
         this would get the list of all the payroll created
     '''
@@ -44,17 +45,34 @@ class ApprovePayroll (APIView):
         has processed it
     '''
     def post (self, request, payroll_id, *args, **kwargs):
-        approval_data = request.data['status']
-        payroll_instance = Payroll.objects.get(id=payroll_id)
-        payroll_instance.status = (approval_data)
-        payroll_instance.save()
-  
-        if approval_data == "INITIALIZED":
-            #send a notification to the operations
-            process_salary_payment(payroll_id)
+        try:
+            # Check if the authenticated user has the required account type.
+            check_account_type(request.user, account_type)
 
-        elif approval_data == "CANCELLED":
-            #send a notification to the operations accountant 
-            pass 
+            approval_data = request.data['status']
+            payroll_instance = Payroll.objects.get(id=payroll_id)
+            payroll_instance.status = (approval_data)
+            payroll_instance.save()
+    
+            if approval_data == "INITIALIZED":
+                #send a notification to the operations
+                process_salary_payment(payroll_id)
 
-        return Response({"message": "An error occurred"}, status=HTTP_200_OK)
+            elif approval_data == "CANCELLED":
+                #send a notification to the operations accountant 
+                pass 
+
+            return Response({"message": "An error occurred"}, status=HTTP_200_OK)
+
+
+        except PermissionDenied:
+            # If the user doesn't have the required permissions, return an HTTP 403 Forbidden response.
+            return Response({"message": "Permission denied"}, status=HTTP_403_FORBIDDEN)
+
+        except APIException as e:
+            # Handle specific API-related errors and return their details.
+            return Response({"message": str(e.detail)}, status=e.status_code)
+
+        except Exception as e:
+            # For all other exceptions, return a generic error message.
+            return Response({"message": "An error occurred"}, status=HTTP_403_FORBIDDEN)
