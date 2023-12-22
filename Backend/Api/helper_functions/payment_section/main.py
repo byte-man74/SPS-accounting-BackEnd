73a5966c11f24aba625319bfb16c42c46c2eda1e
Sheryf Habib <sheryf.534@gmail.com>
2023-12-22 15:58:43 +0100

    updated the get total amount function

--- Backend/Api/helper_functions/payment_section/main.py
+++ Backend/Api/helper_functions/payment_section/main.py
@@ -1,14 +1,23 @@
 
 
 from django.shortcuts import get_object_or_404
+from Backend.Main.models.fees_structure_models import PaymentStatus
 from Main.models.school_operations_models import Student
-
+from django.db.models import Sum
 
 def get_student_id_from_request (payload):
     student = get_object_or_404(Student, student_id=payload)
     return student
+
+
+def get_total_amount_in_debt(students):
+    '''This function iterates through all the students and calculates the total amount of debt. It caches the result.'''
     
+    total_amount_query = PaymentStatus.objects.filter(student__in=students).aggregate(
+        total_amount=Sum('amount_in_debt') + Sum('amount_outstanding')
+    )
 
+    # Extract the total amount from the aggregation result
+    total_amount = total_amount_query['total_amount'] or 0
 
-def get_total_amount_in_debt ():
-    '''this guy would iterate through all the students and get the total amount of how much is actaully in debt. it would save it to cache'''
+    return total_amount
